96c8ee7d83992d962777a5e42b594f8c8e6df814
Neil <luofeng457@163.com>
2017-03-30 03:08:30 +0800

    first edit

---
+++ music/routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+var media = path.join(__dirname,"../public/media");
+/* GET home page. */
+router.get('/', function(req, res, next) {
+	var fs = require('fs');  //'fs':filesystem
+	fs.readdir(media,function(err, names){
+		if(err){
+			console.log(err);
+		}
+		else{
+			res.render('index', { title: 'Music Show',music:names });
+		}
+	});
+    //res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
